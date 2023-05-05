38799ee46612032a7e2a15ed4c0e97e13e8ebf52
Aref Yazdi <arefyazdi4@gmail.com>
2023-05-06 02:16:46 +0330

    Create a DFS wall follower robo

---
+++ mazfinder_DFS/mazfinder_DFS.ino
@@ -0,0 +1,218 @@
+// C++ code
+//
+/*
+  Mazz solver
+  This code is resposible to get two signal from push buttoms
+  and accourding to resault (1 or 0) it will swith the voltage in motors 
+  for limited time it will cuz the motor to routate
+  then it will come back to normal state
+*/
+
+// #define Arduino_h
+
+// #include <stdlib.h>
+// #include <stdbool.h>
+// #include <string.h>
+// #include <math.h>
+
+// #include <avr/pgmspace.h>
+// #include <avr/io.h>
+// #include <avr/interrupt.h>
+
+// #include "binary.h"
+
+#define ARDUINO_MAIN
+#define HIGH 0x1
+#define LOW  0x0
+
+#define INPUT 0x0
+#define OUTPUT 0x1
+
+void pinMode(uint8_t pin, uint8_t mode);
+void digitalWrite(uint8_t pin, uint8_t val);
+int digitalRead(uint8_t pin);
+void delay(unsigned long ms);
+void setup(void);
+void loop(void);
+
+
+// definning pins i/o
+int buttonStateR = 0;
+int buttonStateL = 0;
+
+
+const int inR = 4;
+const int outRp = 6;
+const int outRn = 5;
+
+const int inL = 8;
+const int outLp = 9;
+const int outLn = 10;
+
+// creating n*n matrix for drawing mapp   
+const int H = 100;
+const int W = 100;
+int mazz[H][W];
+
+// directions value  
+const int up = 0;
+const int right = 1;
+const int down = 2;
+const int left = 3;
+
+// turn or moving time quantom 
+const int small_block = 210;  
+const int full_block = 1100; //1980
+const int small_turn = 80;
+const int full_turn = 320;
+
+// variables
+int direction = 0;
+int block = 0;          
+int turn = 0;
+int x = W/2;
+int y = H/2;      
+
+void print_mazz (){
+  for (int i = 0 ; i < W ; i++ )
+  {
+  Serial.println();
+  for (int j=0 ; j<H ;j++)
+    {
+      Serial.print(mazz [i][j]);
+    }
+  }
+}  
+
+void hand_on_wall (){
+    digitalWrite(outRn, HIGH);
+    digitalWrite(outRp, LOW);
+    digitalWrite(outLp, HIGH);
+    digitalWrite(outLn, LOW);
+    delay(small_turn);
+    turn =+ small_turn;
+    digitalWrite(outRp, HIGH);
+    digitalWrite(outRn, LOW);
+    digitalWrite(outLp, HIGH);
+    digitalWrite(outLn, LOW);  
+}
+
+void setup()
+{
+
+  pinMode(inR, INPUT);
+  pinMode(inL, INPUT);
+
+
+  pinMode(outRp, OUTPUT);
+  pinMode(outRn, OUTPUT);
+
+  pinMode(outLp, OUTPUT);
+  pinMode(outLn, OUTPUT);
+
+  // pinMode(LED_BUILTIN, OUTPUT);
+
+  // set a mazz default value -1   
+  for (int i = 0 ; i < W ; i++ )
+  {
+  for (int j=0 ; j<H ;j++)
+    {
+      mazz [i][j] = -1;
+    }
+  }
+}
+
+
+
+void loop()
+{
+  
+  // print_mazz();
+
+  if (turn >= full_turn) {
+    turn %= full_turn;
+    direction = (direction + 1) % 4;       
+  }
+  if (turn <= -full_turn) {
+    turn %= full_turn;
+    turn =- full_turn;
+    direction = (direction - 1) % 4;        
+  }
+
+    
+  // drawing a mazz map  
+  if (block >= full_block) {
+    block %= full_block;
+    mazz[x][y] = direction; 
+    switch (direction) {
+      case 0:        
+        y =- 1;
+        break;
+      case 1:
+        x =+ 1;
+        break;
+      case 2:
+        y =+ 1;
+        break;
+      case 3:
+        x =- 1;
+        break;
+    }       
+    hand_on_wall() ;             
+  }
+
+    
+
+  // read the state of the pushbutton value
+  buttonStateR = digitalRead(inR);
+  buttonStateL = digitalRead(inL);
+  // check if pushbutton is pressed.  if it is, the
+  // buttonState is HIGH
+  if (buttonStateL == HIGH) {
+    // turn LED on
+    // digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(outRn, HIGH);
+    digitalWrite(outRp, LOW);
+    
+    digitalWrite(outLp, HIGH);
+    digitalWrite(outLn, LOW);
+    
+    delay(small_turn);
+    turn =+ small_turn;     
+  } 
+  if (buttonStateR == HIGH) {
+    // turn LED on
+    // digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(outLn, HIGH);
+    digitalWrite(outLp, LOW);
+
+    digitalWrite(outRp, HIGH);
+    digitalWrite(outRn, LOW);
+    
+    delay(small_turn);
+    turn =- small_turn;     
+  }
+  
+  if (buttonStateR == LOW && buttonStateL == LOW) {
+    // start the engines
+    digitalWrite(outRp, HIGH);
+    digitalWrite(outRn, LOW);
+    
+    digitalWrite(outLp, HIGH);
+    digitalWrite(outLn, LOW);
+  
+    delay(small_block); // Delay a little bit to improve simulation performance
+    block += small_block;
+  }
+
+  if (buttonStateR == HIGH && buttonStateL == HIGH){
+    digitalWrite(outLn, HIGH);
+    digitalWrite(outLp, LOW);
+    
+    digitalWrite(outRp, HIGH);
+    digitalWrite(outRn, LOW);
+    
+    delay(full_turn);
+    turn =- full_turn;      
+  }
+}
