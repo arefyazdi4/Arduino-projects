944f1a1693828785990c3168ede38177f9d62326
Aref Yazdi <arefyazdi4@gmail.com>
2023-12-08 19:30:33 +0330

    Update

--- light_following/light_following.ino
+++ light_following/light_following.ino
@@ -11,18 +11,18 @@ void delay(unsigned long ms);
 void setup(void);
 void loop(void);
 
-int outRp = 6;
-int outRn = 5;
+int outRp = 11;
+int outRn = 10;
 
-int outLp = 9;
-int outLn = 10;
+int outLp = 12;
+int outLn = 13;
 
 int moveDeley = 40;
 
-const int blackLineThreshold = 45;
+const int blackLineThreshold = 100;
 
-int trigger_right = 4;
-int trigger_left = 8;
+int trigger_right = 8;
+int trigger_left = 9;
 
 
 const int ldr_left = A0;
@@ -33,19 +33,36 @@ const int cny_right = A3;
 const int cny_center = A4;
 const int cny_left = A5;
 
+const int enum_stop = 0;
+const int enum_hard_left =1;
+const int enum_soft_left = 2;
+const int enum_center = 3;
+const int enum_soft_right = 4;
+const int enum_hard_right = 5;
 
-const int enum_left = 1;
-const int enum_center = 2;
-const int enum_right = 3;
+const int white = 1;
+const int black = 0;
+
+
+void long_move_delay(){
+      delay(moveDeley);
+      delay(moveDeley);
+      delay(moveDeley);
+}
+
+int get_optical_sensor_digital_value(int sensor){
+  int val = analogRead(sensor);
+  if (val > blackLineThreshold){
+    return white;
+  } else {
+    return black;
+  }
+
+}
 
 void leftWheelForward(){
   digitalWrite(outLp, HIGH);
   digitalWrite(outLn, LOW);
-}
-
-void leftWheelBackrward(){
-  digitalWrite(outLn, HIGH);
-  digitalWrite(outLp, LOW);
 }
 
 void rightWheelForward(){
@@ -53,9 +70,29 @@ void rightWheelForward(){
   digitalWrite(outRn, LOW);
 }
 
+void leftWheelBackrward(){
+  digitalWrite(outLp, LOW);
+  digitalWrite(outLn, HIGH);
+}
+
 void rightWheelBackrward(){
-  digitalWrite(outRn, HIGH);
   digitalWrite(outRp, LOW);
+  digitalWrite(outRn, HIGH);
+}
+
+void leftWheelStop(){
+  digitalWrite(outLp, LOW);
+  digitalWrite(outLn, LOW);
+}
+
+void rightWheelStop(){
+  digitalWrite(outRp, LOW);
+  digitalWrite(outRn, LOW);
+}
+
+void doNotMove(){
+  rightWheelStop();
+  leftWheelStop();
 }
 
 void moveForward(){
@@ -69,7 +106,6 @@ void moveRight(){
   leftWheelForward();
 }
 
-
 void moveLeft(){
   leftWheelBackrward();
   rightWheelForward();
@@ -82,10 +118,10 @@ int getWallState(){
   int triggerStateL = digitalRead(trigger_left);
   // check if pushbutton is pressed.  if it is, the
   if (triggerStateL == HIGH && triggerStateR == LOW) {
-      return enum_right;
+      return enum_soft_right;
   } 
   else if (triggerStateR == HIGH && triggerStateL == LOW) {
-       return enum_left;
+       return enum_soft_left;
   }
   else if (triggerStateR == HIGH && triggerStateL == HIGH){
       return enum_center;
@@ -102,11 +138,14 @@ int getLightState(){
    int center = analogRead(ldr_center);
    int right = analogRead(ldr_right);
 
-   if(left < center && left < right){
-      return enum_left;
+    if(center > right && center > left){
+      return enum_center; 
    }
-   else if(right < center && right < left){
-      return enum_right; 
+   else if(left > center && left > right){
+      return enum_soft_left;
+   }
+   else if(right > center && right > left){
+      return enum_soft_right; 
    }
    else{
      return enum_center;
@@ -114,17 +153,83 @@ int getLightState(){
 }
 
 
-int getLineState(){
-     //get sensor readings from our 3 photoresistors
-   int left = analogRead(cny_left);
-   int center = analogRead(cny_center);
-   int right = analogRead(cny_right);
+int is_stop_sign(int center, int left, int right){
+  if(left == black && right == black){
+    return 1;
+  }  
+  else {
+    return 0;
+  }
+}
 
-   if(left < center && left < right){
-      return enum_left;
+int is_stright_line(int center, int left, int right){
+  if(center == black && left == white && right == white) {
+    return 1;
+  } 
+  else {
+    return 0;
+  }
+}
+
+int is_soft_left_turn(int center, int left, int right){
+  if(center == white && left == black && right == white) {
+    return 1; 
+  } 
+  else {
+    return 0;
+  }
+}
+
+int is_hard_left_turn(int center, int left, int right){
+  if(center == black && left == black && right == white) {
+     return 1;
+  } 
+  else {
+    return 0;    
+  }
+}
+
+int is_soft_right_turn(int center, int left, int right){
+  if(center == white && left == white && right == black) {
+     return 1;
+  } 
+  else {
+    return 0;
+  }
+}
+
+int is_hard_right_turn(int center, int left, int right){
+  if(center == black && left == white && right == black) {
+    return 1;
+  } 
+  else {
+    return 0;
+  }
+}
+
+int getLineState(){
+   //get sensor readings from our 3 inferno red sensor
+   int center = get_optical_sensor_digital_value(cny_center);
+   int left = get_optical_sensor_digital_value(cny_left);
+   int right = get_optical_sensor_digital_value(cny_right);
+
+    if(is_stright_line(center, left, right) == true){
+      return enum_center; 
    }
-   else if(right < center && right < left){
-      return enum_right; 
+   else if(is_soft_left_turn(center, left, right) == true){
+      return enum_soft_left;
+   }
+   else if(is_hard_left_turn(center, left, right) == true){
+      return enum_hard_left;
+   }
+   else if(is_soft_right_turn(center, left, right) == true){
+      return enum_soft_right; 
+   }
+   else if(is_hard_right_turn(center, left, right) == true){
+      return enum_hard_right;
+   }
+   else if(is_stop_sign(center, left, right) == true){
+      return enum_stop;
    }
    else{
      return enum_center;
@@ -134,17 +239,29 @@ int getLineState(){
 
 void moveToState(int state){
   switch(state){
-    case enum_left:
-      moveLeft();
+    case enum_stop:
+      doNotMove();
+      delay(moveDeley);
       break;
     case enum_center:
       moveForward();
+      delay(moveDeley);
       break;
-    case enum_right:
+    case enum_soft_left:
+      moveLeft();
+      delay(moveDeley);
+      break;
+    case enum_hard_left:
+      moveLeft();
+      long_move_delay();
+      break;
+    case enum_soft_right:
       moveRight();
+      delay(moveDeley);
       break;
-    default:
-      moveForward();
+    case enum_hard_right:
+      moveLeft();
+      long_move_delay();
       break;
   }
 }
@@ -166,14 +283,16 @@ void setup() {
 }
 
 int getState(){
-  int state = getWallState();
-  if (state == enum_center){ 
-    state = getLineState();
-  }
-  if (state == enum_center){ 
-    state = getLightState();
-  }
-  return state
+  int state = enum_stop;
+  state = getLineState();
+  // if (state == enum_center){   
+  //   state = getWallState();
+  //   if (state == enum_center){ 
+  //     state = getLightState();
+  //   }
+  // }
+  Serial.println(state);
+  return state;
 }
 
 void moveRobot(){
@@ -183,5 +302,4 @@ void moveRobot(){
 
 void loop() {
   moveRobot();
-  delay(moveDeley);
 }
